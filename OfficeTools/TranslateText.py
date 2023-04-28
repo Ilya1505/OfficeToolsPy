90f4b25370e4892a32051cf2b931bf3490353527
Ilya <vesna1505@yandex.ru>
2023-04-28 16:21:40 +0700

    сделал 3 файла на обработку каждого события, подкорректировал меню на панели инструментов

---
+++ OfficeTools/TranslateText.py
@@ -0,0 +1,52 @@
+import io
+import html
+import os.path
+import codecs
+
+import uno
+import unohelper
+
+from com.sun.star.awt.FontWeight import (NORMAL, BOLD)
+from com.sun.star.awt.FontUnderline import (SINGLE, NONE)
+from com.sun.star.awt.FontSlant import (NONE, ITALIC)
+
+from com.sun.star.task import XJobExecutor
+
+class TranslateText(unohelper.Base, XJobExecutor):
+	def __init__(self, ctx):
+		'''Конструктор класса'''
+		# Сохранение контекста компонента для последующего использования 
+		self.ctx = ctx
+	
+	
+	def trigger(self, event):
+		'''Обработчик события'''
+		# Получение объекта рабочего стола
+		desktop = self.ctx.ServiceManager.createInstanceWithContext("com.sun.star.frame.Desktop", self.ctx)
+		# Получение объекта текущего документа
+		document = desktop.getCurrentComponent()
+		# Проверка возможности доступа к тексту документа
+		if not hasattr(document, "Text"):
+			return
+		controller = document.getCurrentController()
+		select = controller.getSelection()
+		count = select.getCount()
+		for i in range(count) :
+			symbol = select.getByIndex(i)
+			theString = symbol.getString()
+			if len(theString)!=0 :
+				#get the XText interface
+				text = document.Text
+				#create an XTextRange at the end of the document
+				tRange = text.End
+				#and set the string
+				tRange.String = str(count)
+		
+		
+# Регистрация реализации службы
+g_ImplementationHelper = unohelper.ImplementationHelper()
+
+g_ImplementationHelper.addImplementation( \
+		TranslateText,                                # Имя класса UNO
+		"org.openoffice.comp.pyuno.exp.TranslateText",# Имя реализации
+		("com.sun.star.task.Job",),)                # Список реализованных служб
